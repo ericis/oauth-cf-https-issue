ebe69527864cd44c9826a14053f43887214edd29
Eric Swanson <eric.ca.swanson@aa.com>
2018-07-23 18:33:22 -0400

    reproduce issue

---
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private static Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+
+	@Value("${my.https.enabled:false}")
+	private boolean isHttpsEnabled;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.
+			authorizeRequests().
+				antMatchers("/anonymous*").anonymous().
+				anyRequest().authenticated().and().
+			oauth2Login();
+		
+		if (this.isHttpsEnabled) {
+			log.info("HTTPS required");
+			http.requiresChannel().anyRequest().requiresSecure();	
+		} else {
+			log.info("HTTPS not required");
+		}
+	}
+}
