c96e09677881013612985df34cde062a1d0c14f7
Eric Swanson <eric.ca.swanson@aa.com>
2018-07-24 09:40:49 -0400

    working version

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -3,16 +3,20 @@ package com.example.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private static Logger log = LoggerFactory.getLogger(SecurityConfig.class);
 
-	@Value("${my.https.enabled:false}")
+	@Value("${my.https.enabled:true}")
 	private boolean isHttpsEnabled;
 
 	@Override
@@ -20,15 +24,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		http.
 			authorizeRequests().
-				antMatchers("/anonymous*").anonymous().
+				antMatchers("/anonymous*", "favicon.ico").anonymous().
 				anyRequest().authenticated().and().
 			oauth2Login();
 		
 		if (this.isHttpsEnabled) {
 			log.info("HTTPS required");
-			http.requiresChannel().anyRequest().requiresSecure().antMatchers("/login*", "/perform_login");
+			http.requiresChannel().anyRequest().requiresSecure().antMatchers("/login");
 		} else {
 			log.info("HTTPS not required");
 		}
+	}
+	
+	@Bean
+	FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+		
+	    final FilterRegistrationBean<ForwardedHeaderFilter> filterRegistrationBean = new FilterRegistrationBean<ForwardedHeaderFilter>();
+	    
+	    filterRegistrationBean.setFilter(new ForwardedHeaderFilter());
+	    filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	    
+	    return filterRegistrationBean;
 	}
 }
